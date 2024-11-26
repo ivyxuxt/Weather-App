4e82bcae3a7b7d1a2d4b759a6495ffe1ba2f66c0
Ivy Xu <ivyxu@ivys-air.haverford.edu>
2024-11-26 14:31:03 -0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// src/components/Footer.jsx
+import React from 'react';
+import './Footer.css'; 
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p> Weather App - © 2021 Ivy Xu. </p>
+    </footer>
+  );
+};
+
+export default Footer;
